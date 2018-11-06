577d1ff977476c8df101a2ad5d61ed446d45f481
TROJANCJ <trojancj@thing3.cs.uwec.edu>
2018-11-05 20:10:01 -0600

    Formatting

--- client.cpp
+++ client.cpp
@@ -20,125 +20,114 @@
 using namespace std;
 
 void error(const char *msg) {
-    perror(msg);
-    exit(0);
+	perror(msg);
+	exit(0);
 }
 
 int main(int argc, char*argv[]) {
 
-  /*
-  ============================
-      SET UP CONNECTION
-  ============================
-  */
+	/*
+	============================
+	SET UP CONNECTION
+	============================
+	*/
 
-  int sockfd, n;
-  struct sockaddr_in serv_addr;
-  struct hostent *server;
+	int sockfd, n;
+	struct sockaddr_in serv_addr;
+	struct hostent *server;
 
-  if (argc < 2) {
-    fprintf(stderr, "usage is %s hostname\n", argv[0]);
-    exit(0);
-  }
-
-  sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  if (sockfd < 0) error("ERROR opening socket");
-  server = gethostbyname(argv[1]);
-  if (server == NULL) {
-    fprintf(stderr, "ERROR, no such host\n");
-    exit(0);
-  }
-  bzero((char *) &serv_addr, sizeof(serv_addr));
-  serv_addr.sin_family = AF_INET;
-  bcopy((char *)server->h_addr,
-        (char *)&serv_addr.sin_addr.s_addr,
-        server->h_length);
-  serv_addr.sin_port = htons(PORT);
-  if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-    error("ERROR connecting");
-  }
-
-  /*
-  ===============================
-      DO STUFF ONCE CONNECTED
-  ===============================
-  */
-  
-  int maxValue;
-  printf("Please enter the max value: ");
-  cin >> maxValue;
-  
-  // Create array and default everything to true
-  //bool bitsetValues[maxValue+1];
-  //memset(bitsetValues, true, sizeof(bitsetValues));
-  
-  bool *bitsetValues = new bool[maxValue + 1];
-  for (int i = 0; i <= maxValue; i++) {
-	  bitsetValues[i] = true;
-  }
-  
-	
-  // Size of the array
-  write(sockfd, &maxValue, sizeof(maxValue));
-  
-
-  int i = 2;
-  cout << "Prime: " << i << "\n";
-  for (int k=i*2; k<=maxValue; k += i) {
-	bitsetValues[k] = false;
-  }
-  cout << "Sent: ";
-  for (int p = 2; p<=maxValue; p++) {
-	if (bitsetValues[p]) {
-		cout << p << " ";
+	if (argc < 2) {
+		fprintf(stderr, "usage is %s hostname\n", argv[0]);
+		exit(0);
 	}
-  }
-  cout << "\n\n";
+
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0) error("ERROR opening socket");
+		server = gethostbyname(argv[1]);
+	if (server == NULL) {
+		fprintf(stderr, "ERROR, no such host\n");
+		exit(0);
+	}
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	bcopy((char *)server->h_addr,
+		(char *)&serv_addr.sin_addr.s_addr,
+		server->h_length);
+	serv_addr.sin_port = htons(PORT);
+	if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+		error("ERROR connecting");
+	}
+
+	/*
+	===============================
+	DO STUFF ONCE CONNECTED
+	===============================
+	*/
   
-  write(sockfd, &i, sizeof(i));
-  write(sockfd, bitsetValues, sizeof(bitsetValues));
+	int maxValue;
+	printf("Please enter the max value: ");
+	cin >> maxValue;
+
+	// Create array and default everything to true
+	bool *bitsetValues = new bool[maxValue + 1];
+	for (int i = 0; i <= maxValue; i++) {
+	  bitsetValues[i] = true;
+	}
   
-   while(true) {
-	  read(sockfd, &i, sizeof(i));
-	  read(sockfd, bitsetValues, sizeof(bitsetValues));
-	  cout << "Received: ";
-	  //int counter = 0;
-	 for (int p = 2; p<=maxValue; p++) {
+	// Size of the array
+	write(sockfd, &maxValue, sizeof(maxValue));
+
+	int i = 2;
+	cout << "Prime: " << i << "\n";
+	for (int k=i*2; k<=maxValue; k += i) {
+		bitsetValues[k] = false;
+	}
+  
+	cout << "Sent: ";
+	for (int p = 2; p<=maxValue; p++) {
 		if (bitsetValues[p]) {
 			cout << p << " ";
 		}
-	  }
-	 // while (counter != 3) {
-		  //if (bitsetValues[p]) {
-			//  cout << p << ", ";
-		//	  counter++;
-	//	  }
-	  //}
-	  //cout << "...," << endl;
-	  cout << "\n";
-	  while (bitsetValues[i] != true && i < maxValue/2) {
-		 i++;
-	  }
-	  if (i >= maxValue/2) {
-		  break;
-	  } else {
-		cout << "Prime: " << i << "\n";		  
-		for (int k=i*2; k<=maxValue; k += i) {
-			bitsetValues[k] = false;
+	}
+	cout << "\n\n";
+  
+	write(sockfd, &i, sizeof(i));
+	write(sockfd, bitsetValues, sizeof(bitsetValues));
+
+	while(true) {
+		read(sockfd, &i, sizeof(i));
+		read(sockfd, bitsetValues, sizeof(bitsetValues));
+		cout << "Received: ";
+		//int counter = 0;
+		for (int p = 2; p<=maxValue; p++) {
+			if (bitsetValues[p]) {
+				cout << p << " ";
+			}
 		}
-	  }
-	  
-	  cout << "Sent: ";
-	  for (int p = 2; p<=maxValue; p++) {
-		if (bitsetValues[p]) {
-			cout << p << " ";
+		cout << "\n";
+		while (bitsetValues[i] != true && i < maxValue/2) {
+			i++;
 		}
-	  }
-	  cout << "\n\n";
+		if (i >= maxValue/2) {
+			break;
+		} else {
+			cout << "Prime: " << i << "\n";		  
+			for (int k=i*2; k<=maxValue; k += i) {
+				bitsetValues[k] = false;
+			}
+		}
 	  
-	  write(sockfd, &i, sizeof(i));
-	  write(sockfd, bitsetValues, sizeof(bitsetValues));
-  }
+		cout << "Sent: ";
+		for (int p = 2; p<=maxValue; p++) {
+			if (bitsetValues[p]) {
+				cout << p << " ";
+			}
+		}
+		cout << "\n\n";
+
+		write(sockfd, &i, sizeof(i));
+		write(sockfd, bitsetValues, sizeof(bitsetValues));
+	}
   
   close(sockfd);
   return 0;
--- server.cpp
+++ server.cpp
@@ -26,130 +26,127 @@
 using namespace std;
 
 void error(const char *msg) {
-  perror(msg);
-  exit(1);
+	perror(msg);
+	exit(1);
 }
 
 int main (int argc, char *argv[]) {
 
-  /*
-  ============================
-      SET UP CONNECTION
-  ============================
-  */
+	/*
+	============================
+	SET UP CONNECTION
+	============================
+	*/
 
-  int sockfd, newsockfd;
-  socklen_t clilen;
-  char buffer[256];
-  struct sockaddr_in serv_addr, cli_addr;
-  int n;
+	int sockfd, newsockfd;
+	socklen_t clilen;
+	char buffer[256];
+	struct sockaddr_in serv_addr, cli_addr;
+	int n;
 
-  // create a socket
-  // socket(int domain, int type, int protocol)
-  sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  if (sockfd < 0) error("ERROR opening socket");
+	// create a socket
+	// socket(int domain, int type, int protocol)
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0) error("ERROR opening socket");
 
-  // clear address structure
-  bzero((char *) &serv_addr, sizeof(serv_addr));
+	// clear address structure
+	bzero((char *) &serv_addr, sizeof(serv_addr));
 
-  // setup the host_addr structure for use in bind call
-  // server byte order
-  serv_addr.sin_family = AF_INET;
+	// setup the host_addr structure for use in bind call
+	// server byte order
+	serv_addr.sin_family = AF_INET;
 
-  // automatically be filled with current hosts IP address
-  serv_addr.sin_addr.s_addr = INADDR_ANY;
+	// automatically be filled with current hosts IP address
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
 
-  // convert short integer value for port must be converted into network byte order
-  serv_addr.sin_port = htons(PORT);
+	// convert short integer value for port must be converted into network byte order
+	serv_addr.sin_port = htons(PORT);
 
-  // bind(ind fd, struct socaddr *local_addr, socklen_t addr_length)
-  // bind() passes file descriptor, the address of the structure,
-  // and the length of the address structure
-  // This bind() call will bind the socket to the current IP address on port: PORT
-  if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-    error("ERROR on binding");
-  }
+	// bind(ind fd, struct socaddr *local_addr, socklen_t addr_length)
+	// bind() passes file descriptor, the address of the structure,
+	// and the length of the address structure
+	// This bind() call will bind the socket to the current IP address on port: PORT
+	if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+		error("ERROR on binding");
+	}
 
-  // This listen() call tells the socket to listen to the incoming connections
-  // The listen() function places all incoming connection into a backlog queue
-  // until accept() call accepts the connection
-  // max backlog queue is 5
-  listen(sockfd, 5);
+	// This listen() call tells the socket to listen to the incoming connections
+	// The listen() function places all incoming connection into a backlog queue
+	// until accept() call accepts the connection
+	// max backlog queue is 5
+	listen(sockfd, 5);
 
-  // the accept() call actually accepts an incoming connections
-  clilen = sizeof(cli_addr);
+	// the accept() call actually accepts an incoming connections
+	clilen = sizeof(cli_addr);
 
-  /* This accept() function will write the connecting client's address info
-  into the address structure and the size of that structer is clilen.
-  The accept() returns a new socket file descriptor for the accepted connection.
-  So, the origial socket file descriptor can continue to be used
-  for accepting new connections while the new socket file descriptor is used for
-  communcicating with the connected client.
-  */
-  newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+	/* This accept() function will write the connecting client's address info
+	into the address structure and the size of that structer is clilen.
+	The accept() returns a new socket file descriptor for the accepted connection.
+	So, the origial socket file descriptor can continue to be used
+	for accepting new connections while the new socket file descriptor is used for
+	communcicating with the connected client.
+	*/
+	newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
 
-  if (newsockfd < 0) {
-	  error("ERROR on accept");
-  }
+	if (newsockfd < 0) {
+		error("ERROR on accept");
+	}
 
-  // printf("server: got connection from %s port %d\n",
-  //          inet_ntoa(cli_addr.sin_addr), ntohs(cli_addr.sin_port));
+	// printf("server: got connection from %s port %d\n",
+	//          inet_ntoa(cli_addr.sin_addr), ntohs(cli_addr.sin_port));
 
-  /*
-  ===============================
-      DO STUFF ONCE CONNECTED
-  ===============================
-  */
+	/*
+	===============================
+	DO STUFF ONCE CONNECTED
+	===============================
+	*/
 
-  // Highest value we are checking to
-  int maxValue;
-  
-  // Read in max value from client
-  read(newsockfd, &maxValue, sizeof(int));
-  
-  bool *bitsetValues = new bool[maxValue + 1];
-  for (int i = 0; i <= maxValue; i++) {
-	  bitsetValues[i] = true;
-  }
-  int i;
-  while(true) {
-	  read(newsockfd, &i, sizeof(i));
-	  read(newsockfd, bitsetValues, sizeof(bitsetValues));
-	  cout << "Received: ";
-	  for (int p = 2; p<=maxValue; p++) {
-		if (bitsetValues[p]) {
-			cout << p << " ";
+	// Highest value we are checking to
+	int maxValue;
+
+	// Read in max value from client
+	read(newsockfd, &maxValue, sizeof(int));
+
+	bool *bitsetValues = new bool[maxValue + 1];
+	for (int i = 0; i <= maxValue; i++) {
+		bitsetValues[i] = true;
+	}
+	int i;
+	while(true) {
+		read(newsockfd, &i, sizeof(i));
+		read(newsockfd, bitsetValues, sizeof(bitsetValues));
+		cout << "Received: ";
+		for (int p = 2; p<=maxValue; p++) {
+			if (bitsetValues[p]) {
+				cout << p << " ";
+			}
 		}
-	  }
-	  cout << "\n";
-	  i++;
-	  while (bitsetValues[i] != true && i < maxValue/2) {
-		 i++;
-	  }
-	  if (i >= maxValue/2) {
-		  break;
-	  } else {
-		cout << "Prime: " << i << "\n";
-		for (int k=i*2; k<=maxValue; k += i) {
-			bitsetValues[k] = false;
+		cout << "\n";
+		i++;
+		while (bitsetValues[i] != true && i < maxValue/2) {
+			i++;
 		}
-	  }
+		if (i >= maxValue/2) {
+			break;
+		} else {
+			cout << "Prime: " << i << "\n";
+			for (int k=i*2; k<=maxValue; k += i) {
+				bitsetValues[k] = false;
+			}
+		}
 	  
-	  cout << "Sending: ";
-	  for (int p = 2; p<=maxValue; p++) {
-		if (bitsetValues[p]) {
-			cout << p << " ";
+		cout << "Sending: ";
+		for (int p = 2; p<=maxValue; p++) {
+			if (bitsetValues[p]) {
+				cout << p << " ";
+			}
 		}
-	  }
-	  cout << "\n\n";
-	  
-	  write(newsockfd, &i, sizeof(i));
-	  write(newsockfd, bitsetValues, sizeof(bitsetValues));
-  }
-  
-  
-  close(newsockfd);
-  close(sockfd);
-  return 0;
+		cout << "\n\n";
 
+		write(newsockfd, &i, sizeof(i));
+		write(newsockfd, bitsetValues, sizeof(bitsetValues));
+	}
+	close(newsockfd);
+	close(sockfd);
+	return 0;
 }
